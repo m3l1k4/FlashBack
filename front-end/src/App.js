27fbfa3d9ae41c5543a643cb71926a8e0bdb61d0
m3l1k4 <melikaihelas@gmail.com>
2020-07-17 20:05:13 -0700

    key press detection

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ArrowKeysReact from 'arrow-keys-react';
+ 
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      content: 'Use arrow keys on your keyboard!'
+    };
+    ArrowKeysReact.config({
+      left: () => {
+        // this.setState({
+        //   content: 'left key detected.'
+        // });
+        leftTest();
+      },
+      right: () => {
+        this.setState({
+          content: 'right key detected.'
+        });
+      },
+      up: () => {
+        this.setState({
+          content: 'up key detected.'
+        });
+      },
+      down: () => {
+        this.setState({
+          content: 'down key detected.'
+        });
+      }
+    });
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  
+  render() {
+    return (
+      <div {...ArrowKeysReact.events} tabIndex="1">
+        {this.state.content}
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
+ 
+
+export function leftTest(){
+  console.log("left")
+}
