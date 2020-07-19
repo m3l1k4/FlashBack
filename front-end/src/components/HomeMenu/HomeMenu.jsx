132bce1757f711d5b5b19c80e49d0e7286941a5f
m3l1k4 <melikaihelas@gmail.com>
2020-07-19 14:55:55 -0700

    homepage styligns

--- front-end/src/components/HomeMenu/HomeMenu.jsx
+++ front-end/src/components/HomeMenu/HomeMenu.jsx
@@ -1,8 +1,22 @@
 
 import React, { Component } from "react";
+import './HomeMenu.scss'
 
+export default function HomeMenu() {
 
-export default function HomeMenu(){
+    return (
 
-    return(<h1 className ="score"> HomeMenu: </h1>)
+        <div className="HomeMenu">
+
+            <div className="HomeMenu__sidebar">
+            <img className="HomeMenu__sidebar--logo" src="../assets/logo.png" ></img>
+            </div>
+
+            <div className="HomeMenu__maindiv">
+            <img className="HomeMenu__maindiv--icon" src="../assets/icons/ABOUT.svg" ></img>
+            <img className="HomeMenu__maindiv--icon" src="../assets/icons/GAMES.svg" ></img>
+            </div>
+        </div>
+
+    )
 }
