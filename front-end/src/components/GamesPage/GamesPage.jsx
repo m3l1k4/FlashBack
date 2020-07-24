c20822dd2f382e2f381bba6c37aa5d1b438c9bf2
m3l1k4 <melikaihelas@gmail.com>
2020-07-23 19:35:05 -0700

    stylings

--- front-end/src/components/GamesPage/GamesPage.jsx
+++ front-end/src/components/GamesPage/GamesPage.jsx
@@ -7,8 +7,24 @@ export default function GamesPage() {
     return (
 
         <div className="GamesPage">
-games
-          
+            <div className="GamesPage__sidebar">
+                <a href="/">   <img className="GamesPage__sidebar--logo" src="../assets/logo.png" ></img></a>
+
+                <div className="GamesPage__sidebar--options">
+
+                    <a href="/about"><img className="GamesPage__sidebar--options__icon" src="../assets/icons/ABOUT.svg" ></img></a>
+                    <a href="/games"> <img className="GamesPage__sidebar--options__icon" src="../assets/icons/GAMES.svg" ></img></a>
+                </div>
+            </div>
+
+
+            <div className="GamesPage__main">
+            <a href="/DDR">   <img  className="GamesPage__main--sign__one" src="../assets/AD.png" ></img></a>
+            <a href="#">   <img  className="GamesPage__main--sign__two" src="../assets/neon.png" ></img></a>
+            <a href="#">   <img  className="GamesPage__main--sign__three" src="../assets/neon.png" ></img></a>
+            </div>
+
+
         </div>
 
     )
