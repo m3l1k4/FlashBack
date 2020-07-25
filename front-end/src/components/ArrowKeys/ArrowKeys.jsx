9a719fee4d592c2e9c6d2bdf7b3fb96f8e70064d
m3l1k4 <melikaihelas@gmail.com>
2020-07-25 14:34:31 -0700

    soundcloud api embed

--- front-end/src/components/ArrowKeys/ArrowKeys.jsx
+++ front-end/src/components/ArrowKeys/ArrowKeys.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import './ArrowKeys.scss'
 
-
+// import '../SoundCloud/SoundCloud.js'
 
 class ArrowKeys extends Component {
 
@@ -13,17 +13,24 @@ class ArrowKeys extends Component {
         // this.div.appendChild(script);
 
 
-        const script2 = document.createElement("script");
-        script2.async = true;
-        script2.src = './jsRev.js';
-        this.div.appendChild(script2);
+        // const script2 = document.createElement("script");
+        // script2.async = true;
+        // script2.src = './jsRev.js';
+        // this.div.appendChild(script2);
+
+
+
+        const script3 = document.createElement("script");
+        script3.async = true;
+        script3.src = '../SoundCloud/SoundCloud.js';
+        this.div.appendChild(script3);
 
     }
 
 
     render() {
 
-   
+
         return (
 
 
@@ -32,23 +39,33 @@ class ArrowKeys extends Component {
                 <div className="ArrowKeys" ref={el => (this.div = el)}>
 
                     <div>
-                    <div id="controls">
-                        <img id="left" src="../assets/arrows/staticLeft.gif" width="66px" height="66px"></img>
-                        <img id="up" src="../assets/arrows/staticUp.gif" width="66px" height="66px"></img>
-                        <img id="down" src="../assets/arrows/staticDown.gif" width="66px" height="66px"></img>
-                        <img id="right" src="../assets/arrows/staticRight.gif" width="66px" height="66px"></img>
-                    </div>
-                    <div className="stage"></div>
+                        <div id="controls">
+                            <img id="left" src="../assets/arrows/staticLeft.gif" width="66px" height="66px"></img>
+                            <img id="up" src="../assets/arrows/staticUp.gif" width="66px" height="66px"></img>
+                            <img id="down" src="../assets/arrows/staticDown.gif" width="66px" height="66px"></img>
+                            <img id="right" src="../assets/arrows/staticRight.gif" width="66px" height="66px"></img>
+                        </div>
+                        <div className="stage"></div>
                     </div>
                     <div div className="score">
 
-                    <h1 className="score--text">S</h1>
-                    <h1 className="score--text">C</h1>
-                    <h1 className="score--text">O</h1>
-                    <h1 className="score--text">R</h1>
-                    <h1 className="score--text">E</h1>
-                    <div className="board"></div>
-                </div>
+                        <h1 className="score--text">S</h1>
+                        <h1 className="score--text">C</h1>
+                        <h1 className="score--text">O</h1>
+                        <h1 className="score--text">R</h1>
+                        <h1 className="score--text">E</h1>
+                        <div className="board"></div>
+               
+                    </div>
+
+                    <div className="soundCloud">
+                            <iframe width="100%" height="100" scrolling="no"
+                                frameborder="no" allow="autoplay"
+                                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1098848317&color=%239b3ee3&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false">
+                                    
+                                </iframe>
+                         
+                        </div>
                 </div>
             </div>
         );
