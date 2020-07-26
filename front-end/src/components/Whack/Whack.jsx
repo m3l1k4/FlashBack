96d55ace8fa916724be7a3019374c18cc4f92be7
m3l1k4 <melikaihelas@gmail.com>
2020-07-25 17:55:24 -0700

    whack init

---
+++ front-end/src/components/Whack/Whack.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+// import './Whack.scss'
+
+// import '../SoundCloud/SoundCloud.js'
+
+class Whack extends Component {
+
+
+    render() {
+
+
+        return (
+
+
+<div className="Whack">hello</div>
+        );
+    }
+
+}
+
+
+
+export default Whack;
---
+++ front-end/src/components/Whack/index.js
@@ -0,0 +1,57 @@
+// var quack = new Audio("./assets/quack.mp3")
+
+export {default} from './Whack'
+
+// var score = 0;
+
+// setInterval(startWhack, 995)
+
+
+// document.getElementById('mole').addEventListener("click", function (event) {
+//     event.preventDefault();
+//     score = score + 1;
+//     document.getElementById('score').innerHTML = score;
+//     quack.play();
+
+// });
+
+
+// function startWhack() {
+
+//     let xCordf = getRandomX(20, 1200);
+//     let xCord = float2int(xCordf);
+//     let yCordf = getRandomY(10, 500);
+//     let yCord = float2int(yCordf);
+//     let diditWork = whackedMole(xCord, yCord);
+
+
+//     return
+
+// }
+
+
+// function whackedMole(x, y) {
+//     let xString = '';
+//     let yString = '';
+
+//     let randX = xString.concat(x, 'px');
+//     let randY = yString.concat(y, 'px');
+//     document.getElementById("mole").style.top = randY;
+//     document.getElementById("mole").style.left = randX;
+
+//     return "potato";
+// }
+
+// function getRandomX(min, max) {
+//     return Math.random() * (max - min) + min;
+// }
+
+// function getRandomY(min, max) {
+//     return Math.random() * (max - min) + min;
+// }
+
+// function float2int(value) {
+
+//     return (value | 0);
+// }
+
