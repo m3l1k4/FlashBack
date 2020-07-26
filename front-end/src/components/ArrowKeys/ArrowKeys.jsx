9e553ca6d3dd918288e396b808d0d0a18d16d67a
m3l1k4 <melikaihelas@gmail.com>
2020-07-25 17:45:32 -0700

    code cleanup

--- front-end/src/components/ArrowKeys/ArrowKeys.jsx
+++ front-end/src/components/ArrowKeys/ArrowKeys.jsx
@@ -7,23 +7,13 @@ class ArrowKeys extends Component {
 
 
     componentDidMount() {
-        // const script = document.createElement("script");
-        // script.async = true;
-        // script.src = "http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js";
-        // this.div.appendChild(script);
 
 
-        // const script2 = document.createElement("script");
-        // script2.async = true;
-        // script2.src = './jsRev.js';
-        // this.div.appendChild(script2);
+        const script2 = document.createElement("script");
+        script2.async = true;
+        script2.src = './jsRev.js';
+        this.div.appendChild(script2);
 
-
-
-        const script3 = document.createElement("script");
-        script3.async = true;
-        script3.src = '../SoundCloud/SoundCloud.js';
-        this.div.appendChild(script3);
 
     }
 
@@ -62,19 +52,6 @@ class ArrowKeys extends Component {
   <span class="instructext">Press P for Pause. To listen to music, pause game, start the DJ, then resume! Have fun!</span>
 </div>
                     </div>
-
-
-                   
-
-                    {/* <div className="soundCloud">
-   
-                            <iframe className="soundCloud__iframe"width="200" height="100" scrolling="no"
-                                frameborder="no" allow="autoplay"
-                                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1098848317&color=%239b3ee3&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false">
-                                    
-                                </iframe>
-                                <img  className="soundCloud__img"src="../assets/juke-box-neon.png" ></img>
-                        </div> */}
                 </div>
             </div>
         );
