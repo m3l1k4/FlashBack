51b223fe759511537e5256f2c14e94f0337f2cdf
m3l1k4 <melikaihelas@gmail.com>
2020-07-27 10:56:15 -0700

    url path updated

--- front-end/src/components/GamesPage/GamesPage.jsx
+++ front-end/src/components/GamesPage/GamesPage.jsx
@@ -11,7 +11,7 @@ export default function GamesPage() {
             <div className="GamesPage__main">
             <a href="/DDR">   <img  className="GamesPage__main--sign__one" src="../assets/AD.png" ></img></a>
             <a href="/piano">   <img  className="GamesPage__main--sign__two" src="../assets/piano.png" ></img></a>
-            <a href="/#">   <img  className="GamesPage__main--sign__three" src="../assets/neon.png" ></img></a>
+            <a href="/whack">   <img  className="GamesPage__main--sign__three" src="../assets/neon.png" ></img></a>
             </div>
 
 
